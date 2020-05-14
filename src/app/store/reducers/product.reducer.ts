8ee89f65162a951652e1a930a8ad2c2daf084d71
Bennett <bennybenester@gmail.com>
2020-05-14 22:18:31 +0300

    :sparkles: Add an initial state

--- src/app/store/reducers/product.reducer.ts
+++ src/app/store/reducers/product.reducer.ts
@@ -1,14 +1,19 @@
-import { state } from 'src/app/interfaces/state.interface'
-import { ProductState } from 'src/app/interfaces/product.interface'
+import { ProductState } from "src/app/interfaces/product.interface";
 
-export function reducer(state: ProductState, action): ProductState{
-switch(action.type){
-  case 'TOGGLE_PRODUCT_CODE':
-  return {
-    ...state,
-    showProductCode: action.payload
+const initialstate: ProductState = {
+  showProductCode: true,
+  currentProduct: null,
+  products: [],
+};
+
+export function reducer(state = initialstate, action): ProductState {
+  switch (action.type) {
+    case "TOGGLE_PRODUCT_CODE":
+      return {
+        ...state,
+        showProductCode: action.payload,
+      };
+    default:
+      return state;
   }
-  default:
-  return state
-}
 }
