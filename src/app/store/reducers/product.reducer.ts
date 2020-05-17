f6b5d4253f7f64f706e2d8e34bb1d31ac1991bc1
Bennett <bennybenester@gmail.com>
2020-05-17 23:50:56 +0300

    :sparkles: Add all remaining action types in reducer

--- src/app/store/reducers/product.reducer.ts
+++ src/app/store/reducers/product.reducer.ts
@@ -1,5 +1,5 @@
 import { ProductState } from "src/app/interfaces/product.interface";
-import { ProductActions, ProductActionTypes } from '../actions/product.action';
+import { ProductActions, ProductActionTypes } from "../actions/product.action";
 
 const initialstate: ProductState = {
   showProductCode: true,
@@ -7,12 +7,35 @@ const initialstate: ProductState = {
   products: [],
 };
 
-export function reducer(state = initialstate, action: ProductActions): ProductState {
+export function reducer(
+  state = initialstate,
+  action: ProductActions
+): ProductState {
   switch (action.type) {
     case ProductActionTypes.ToggleProductCode:
       return {
         ...state,
         showProductCode: action.payload,
+      };
+    case ProductActionTypes.SetCurrentProduct:
+      return {
+        ...state,
+        currentProduct: { ...action.payload },
+      };
+
+    case ProductActionTypes.ClearCurrentProduct:
+      return { ...state, currentProduct: null };
+
+    case ProductActionTypes.InitializeCurrentProduct:
+      return {
+        ...state,
+        currentProduct: {
+          id: 0,
+          productCode: "New",
+          productName: "",
+          description: "",
+          starRating: 0,
+        },
       };
     default:
       return state;
